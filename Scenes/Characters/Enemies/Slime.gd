3e8f94f519e24db9c5d4ad00dab18fccfcbb2b57
enterye <nalehulsey@gmail.com>
2023-08-23 17:55:22 -0500

    slime update

--- Scenes/Characters/Enemies/Slime.gd
+++ Scenes/Characters/Enemies/Slime.gd
@@ -9,17 +9,37 @@ var main_character
 @export var can_jump = true
 @export var jumping = false
 
+func _ready():
+	$SlimeSprite.visible = true
+	$Shadow.visible = true
+	$Collisions.disabled = false
+	$Hurtbox/CollisionShape2D.disabled = false
+	$Death.emitting = false
+
 func _process(_delta):
 	match(state):
 		"idle":
 			pass
 		"attack":
 			$AnimationPlayer.play("jump")
+		"dead":
+			pass
 			
 	move_and_slide()
 
 func _on_hitbox_area_entered(_area):
-	print("slime damage taken")
+	health -= 50
+	print(health)
+	if health < 1:
+		die() 
+
+func die():
+	nullify_velocity()
+	$AnimationPlayer.play("die")
+	state = "dead"
+
+func nullify_velocity():
+	velocity = Vector2.ZERO
 
 func jump():
 	velocity = (main_character.global_position - global_position).normalized() * 75
